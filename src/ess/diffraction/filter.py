1b91e4affff84815e635d4b99db3bae207ee12cd
Samuel Jones <samuel.jones@stfc.ac.uk>
2021-10-05 15:45:36 +0100

    Finalise filter draft without all tests

--- src/ess/diffraction/filter.py
+++ src/ess/diffraction/filter.py
@@ -2,30 +2,178 @@
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
 
 import scipp as sc
+import string
+from random import random
+
+import numpy as np
 
 
-def filter_bad_pulses(da: sc.DataArray, centre=False):
+def filter_bad_pulses(da: sc.DataArray, centre: bool = False, minimum_threshold: float = .95):
+    """
+    Filters out any bad pulses based on an attribute named "proton_charge" if the "proton_charge" value is less than 95%
+     of the mean (by default, the percentage can be changed using "minimum_threshold") it will be filtered out, if it is
+     10% higher than the maximum (This won't actually make any difference in comparison to just the max value) based on
+     how Mantid did filtering of bad pulses.
+
+    Args:
+        da: sc.DataArray, the data array that is to be filtered.
+
+        centre: bool, whether or not to centre the data of the "proton_charge" as if it were binned, this is done with
+         SNS data.
+
+        minimum_threshold: float, the minimum threshold is a percentage used calculate at what point is determined as a
+         bad proton charge, this value is multiplied by the mean value of the proton_charge data, and that is the minimum
+         by which filtering will occur.
+
+    Returns:
+    A DataArray that contains a dimension called "pulse_slices" in which "good" data in slice 1, and "bad" data in slice
+    "0". To access the good data you would slice this return result like this: da["pulse_slices", 1].
+    """
     proton_charge = da.attrs['proton_charge'].value
     proton_charge = proton_charge.rename_dims({'time': 'pulse_time'})
     proton_charge.coords['pulse_time'] = proton_charge.coords.pop('time')
 
     if centre:
         proton_charge.data.values[:-1] = 0.5 * (proton_charge.data.values[1:] + proton_charge.data.values[:-1])
-        # proton_charges -= sc.mean(0.5 * (proton_charges["pulse_time", 1:] - proton_charges["pulse_time", :-1]))
 
     max_charge = sc.max(proton_charge.data)
     max_charge *= 1.1  # determine true max (max_value * 1.1)
     min_charge = sc.mean(proton_charge.data)
-    min_charge *= .95  # determine true min (mean_value * .01)
+    min_charge *= minimum_threshold  # determine true min (mean_value * .01)
 
     good_pulse = (proton_charge >= min_charge) & (proton_charge < max_charge)
 
     return filter_data_array_on_attribute(da=da, good_data_lut=good_pulse, mapping_coord="pulse_time",
-                                          good_data_slice_name="good_pulse")
+                                          data_slices_name="pulse_slices")
+
+
+def _input_processing_time_conversion(start, end, datetime_unit):
+    if not isinstance(start, sc.Variable):
+        start = sc.scalar(value=np.datetime64(start).astype(f"datetime64[{datetime_unit}]"))
+    if not isinstance(end, sc.Variable):
+        end = sc.scalar(value=np.datetime64(end).astype(f"datetime64[{datetime_unit}]"))
+    return start, end
+
+
+def filter_by_time(da: sc.DataArray, start, end, time_dim_name: str = "pulse_time", datetime_unit: str = "ns"):
+    """
+    Snip out or slice out a part of a DataArray of binned events, based on time, given start and end
+
+    Args:
+        da: sc.DataArray, the sc.DataArray that is being filtered
+
+        start: Any convertible to a datetime64 Scalar variable e.g. str or sc.Variable, input using absolute time format
+         similar to Mantid, e.g. 2011-08-13T17:11:30 or any other format that can be converted into a numpy datetime64
+         variable. The start of the time_dim_name dimension, in the output
+
+        end: Any, input format the same as start. The end of the time_dim_name dimension in the output
+
+        time_dim_name: str, The time dimension name, defaults to "pulse_time"
+
+        datetime_unit: str, The time unit used in the time_dim_name dimension, e.g. nanoseconds = "ns" or seconds = "s",
+          defaults to "ns". Only needed if you pass not already constructed start or end i.e. a string instead of a
+          Scalar variable containing datetime64.
+
+    Returns:
+        The output of the sc.bin after filtering out everything between the start and end args from the input da
+    """
+    start, end = _input_processing_time_conversion(start, end, datetime_unit)
+    edges = sc.concatenate(start, end, time_dim_name)
+    return sc.bin(da, edges=[edges])
+
+
+# Example of how you would filter an attribute by time on a given DataArray
+def filter_attribute_by_time(da: sc.DataArray, attribute_name: str, start, end, time_name: str = "time",
+                             datetime_unit: str = "ns"):
+    """
+    Return all data points between a minimum and maximum time of an attribute in a DataArray
+
+    Args:
+        da: sc.DataArray, the sc.DataArray that is being filtered
+
+        attribute_name: str, The name of the attribute to be filtered from the DataArray da.
+
+        start: Any convertible to a datetime64 Scalar variable e.g. str or sc.Variable, input using absolute time format
+         similar to Mantid, e.g. 2011-08-13T17:11:30 or any other format that can be converted into a numpy datetime64
+         variable. The start of the time_dim_name dimension, in the output
+
+        end: Any, input format the same as start. The end of the time_dim_name dimension in the output
+
+        time_name: str, The name of the time coordinate in the attribute, defaults to "time"
+
+        datetime_unit: str, The time unit used in the time_dim_name dimension, e.g. nanoseconds = "ns" or seconds = "s",
+          defaults to "ns". Only needed if you pass not already constructed start or end i.e. a string instead of a
+          Scalar variable containing datetime64.
+
+    Returns:
+        sc.DataArray or sc.Variable, whichever the attribute is in the original da that is passed.
+    """
+    start, end = _input_processing_time_conversion(start, end, datetime_unit)
+    attribute_data = da.attrs[attribute_name].value
+    return attribute_data[time_name, start:end]
+
+
+def _generate_random_string(length: int = 20):
+    charecters = string.ascii_letters + string.digits + string.punctuation
+    return "".join(random.choice(charecters) for ii in range(length))
+
+
+def filter_attribute_by_value(da: sc.DataArray, attribute_name: str, min_value: sc.Variable, max_value: sc.Variable):
+    """
+    Return all data points between a minimum and maximum values of an attribute in a DataArray
+
+    Args:
+        da: sc.DataArray, the sc.DataArray that is being filtered
+
+        attribute_name: str, The name of the attribute to be filtered from the DataArray da.
+
+        min_value: sc.Variable, the lower bound of the filtering for the attribute
+
+        max_value: sc.Variable, the upper bound of the filtering for the attribute
+
+    Returns:
+         sc.DataArray or sc.Variable, whichever the attribute is in the original da that is passed.
+    """
+    # Define what data is in range
+    attribute = da.attrs[attribute_name].value
+    greater_than = attribute > min_value
+    less_than = attribute < max_value
+    in_range = greater_than == less_than
+
+    # Group up data based on the in range data that is present and clean up groupby coordinate
+    filter_coord_name = _generate_random_string()
+    attribute.coords[filter_coord_name] = in_range.data
+    data_in_range = attribute.groupby(filter_coord_name).copy(1)
+    del data_in_range.coords[filter_coord_name]
+    del attribute.coords[filter_coord_name]
+
+    return data_in_range
 
 
 def filter_data_array_on_attribute(da: sc.DataArray, good_data_lut, mapping_coord: str,
-                                   good_data_slice_name="good_data"):
+                                   data_slices_name: str = "slice_data"):
+    """
+    Filter the data based on the passed good_data_lut, output the passed DataArray with a new dimension called the
+    string passed to data_slices_name, where the data in slice 0, is "bad" data and the data in slice 1 is the "good"
+    data.
+
+    Args:
+        da: sc.DataArray, the sc.DataArray that is being filtered
+
+        good_data_lut: Any, a definition of what is good data, using the boolean operators on a set of variables. For
+         example this is achieved in "filter_bad_pulses" by (proton_charge >= min_charge) & (proton_charge < max_charge)
+         where proton_charge is the attribute (sc.Variable) being filtered.
+
+        mapping_coord: str, The coordinate that connects the attribute, and the data together, this is often the,
+         pulse_time, you may need to rename the attribute's coordinate to support this.
+
+        data_slices_name: str, The name of the dimension that will be added to the passed sc.DataArray, da, that can
+         then be sliced later for good and bad data.
+
+    Returns:
+        The originally passed sc.DataArray, da, with an extra dimension that can then be sliced for good data on slice
+        1, and sliced for bad data on slice 0. For example, da["slice_data", 1] for good data.
+    """
     good_data_coord = good_data_lut.coords[mapping_coord]
     edge = good_data_lut.data
     edge = edge[mapping_coord, :-1] ^ edge[mapping_coord, 1:]
@@ -33,15 +181,14 @@ def filter_data_array_on_attribute(da: sc.DataArray, good_data_lut, mapping_coor
         data={mapping_coord: good_data_coord[mapping_coord, 1:], 'good': good_data_lut.data[mapping_coord, :-1]},
         coords={'edge': edge})
     good_edges = good_edges.groupby(group='edge').copy(1)
-    good_pulse2 = sc.DataArray(data=good_edges['good'].data ^
-                                    good_data_lut[mapping_coord, 0].data,
-                               coords={mapping_coord: good_edges[mapping_coord].data})
-    good_pulse2 = sc.concatenate(good_data_lut[mapping_coord, 0], good_pulse2,
-                                 mapping_coord)
-    good_pulse2 = sc.concatenate(good_pulse2, good_data_lut[mapping_coord, -1],
-                                 mapping_coord)
-    good_pulse_lut = sc.lookup(good_pulse2, mapping_coord)
-    da.bins.coords[good_data_slice_name] = good_pulse_lut[da.bins.coords[mapping_coord]]
-    groups = sc.array(dims=[good_data_slice_name], values=[False, True])
+
+    good_data = sc.DataArray(data=good_edges['good'].data ^ good_data_lut[mapping_coord, 0].data,
+                             coords={mapping_coord: good_edges[mapping_coord].data})
+    good_data = sc.concatenate(good_data_lut[mapping_coord, 0], good_data, mapping_coord)
+    good_data = sc.concatenate(good_data, good_data_lut[mapping_coord, -1], mapping_coord)
+    good_pulse_lut = sc.lookup(good_data, mapping_coord)
+
+    da.bins.coords[data_slices_name] = good_pulse_lut[da.bins.coords[mapping_coord]]
+    groups = sc.array(dims=[data_slices_name], values=[False, True])
     grouped = sc.bin(da, groups=[groups])
     return grouped
--- tests/diffraction/filter_test.py
+++ tests/diffraction/filter_test.py
@@ -1,53 +1,253 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (c) 2021 Scipp contributors (https://github.com/scipp)
-
+import numpy as np
 import scipp as sc
 import scippneutron as scn
 
-from benchmarking.bench_function import bench_scipp_filter, bench_mantid_filter
-from ess.diffraction.filter import filter_bad_pulses
-from tests.utils import with_mantid_only
+import pytest
+
+from ess.diffraction.filter import filter_bad_pulses, filter_by_time, filter_attribute_by_time
 
 
-@with_mantid_only
-def test_result_of_bad_pulse():
-    from mantid.simpleapi import Load, FilterBadPulses
+def _test_filter_sums_for_bad_pulses(filtered_da, bad_sum, good_sum):
+    good_filtered_da = filtered_da["pulse_slices", 1]
+    bad_filtered_da = filtered_da["pulse_slices", 0]
+    good_da_sum = sc.sum(good_filtered_da.bins.sum()).value
+    bad_da_sum = sc.sum(bad_filtered_da.bins.sum()).value
 
-    # Example script
-    filename = "PG3_4866_event.nxs"
-    ws = Load(filename)
-    ds = scn.from_mantid(ws)
-
-    # Perform scipp filter
-    scipp_centre_filtered_ds = filter_bad_pulses(ds.copy(), True)["good_pulse", 1]
-    scipp_centre_sum = sc.sum(scipp_centre_filtered_ds.bins.sum())
-    scipp_left_filtered_ds = filter_bad_pulses(ds.copy(), False)["good_pulse", 1]
-    scipp_left_sum = sc.sum(scipp_left_filtered_ds.bins.sum())
-
-    # Perform mantid filter
-    ws = FilterBadPulses(InputWorkspace=ws)
-    mantid_filtered_ds = scn.from_mantid(ws)
-    mantid_sum = sc.sum(mantid_filtered_ds.bins.sum())
-
-    scipp_centre_diff = abs(scipp_centre_sum.value - mantid_sum.value)
-    scipp_left_diff = abs(scipp_left_sum.value - mantid_sum.value)
-
-    assert scipp_centre_diff == 524.0
-    assert scipp_left_diff == 619484.0
+    np.testing.assert_almost_equal(good_da_sum, good_sum)
+    np.testing.assert_almost_equal(bad_da_sum, bad_sum)
 
 
-@with_mantid_only
-def test_result_of_bad_pulse_performance_mantid_comparison():
-    from mantid.simpleapi import FilterBadPulses
+def test_results_of_bad_pulse_defaults():
+    da = scn.data.tutorial_event_data()
 
-    scipp_centre = bench_scipp_filter(filter_bad_pulses, filename="PG3_4866_event.nxs", repeat_times=5, args=True)
-    scipp_left = bench_scipp_filter(filter_bad_pulses, filename="PG3_4866_event.nxs", repeat_times=5)
-    mantid_centre = bench_mantid_filter(FilterBadPulses, filename="PG3_4866_event.nxs", repeat_times=5)
+    filtered_da = filter_bad_pulses(da)
 
-    average_scipp = (float(scipp_left) + float(scipp_centre)) / 2.
+    _test_filter_sums_for_bad_pulses(filtered_da, 51799.703125, 6282525.0)
 
-    # Assert that Mantid is no more than 3 times faster than the average of a Scipp equivalent operation for filtering
-    # bad pulses, this is for stability as we expect no more than 2 times as more Scipp operations need to occur.
-    assert mantid_centre / average_scipp <= 3
+
+def test_results_of_bad_pulse_centred():
+    da = scn.data.tutorial_event_data()
+
+    filtered_da = filter_bad_pulses(da, True)
+
+    _test_filter_sums_for_bad_pulses(filtered_da, 120990.6640625, 6213332.0)
+
+
+def test_results_of_bad_pulse_left():
+    da = scn.data.tutorial_event_data()
+
+    filtered_da = filter_bad_pulses(da, False)
+
+    _test_filter_sums_for_bad_pulses(filtered_da, 51799.703125, 6282525.0)
+
+
+def test_results_of_bad_pulse_min_threshold():
+    da = scn.data.tutorial_event_data()
+
+    filtered_da = filter_bad_pulses(da, minimum_threshold=.5)
+
+    _test_filter_sums_for_bad_pulses(filtered_da, 23172.6582031, 6311146.5)
+
+
+def test_result_of_filter_by_time_first_half():
+    da = scn.data.tutorial_event_data()
+
+    # Run scipp filter by time
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+    run_end = run_start + sc.to_unit(
+        sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+        unit="ns")
+    filtered_da = filter_by_time(da.copy(), run_start, run_end)
+    da_sum = sc.sum(filtered_da.bins.sum())
+
+    assert da_sum.value == 3317887.25
+
+
+def test_result_of_filter_by_time():
+    da = scn.data.tutorial_event_data()
+
+    # Run scipp filter by time
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+    half_duration = sc.to_unit(sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+                               unit="ns")
+    run_end = run_start + half_duration
+    filtered_da = filter_by_time(da.copy(), run_start, run_end)
+    da_sum = sc.sum(filtered_da.bins.sum())
+
+    assert da_sum.value == 3317887.25
+
+
+def test_result_of_filter_by_time_different_event_time_coord():
+    da = scn.data.tutorial_event_data()
+
+    # Rename time in the data
+    new_dim_name = "time"
+    da.bins.coords[new_dim_name] = da.bins.coords.pop('pulse_time')
+
+    # Run scipp filter by time
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+    half_duration = sc.to_unit(sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+                               unit="ns")
+    run_end = run_start + half_duration
+    filtered_da = filter_by_time(da.copy(), run_start, run_end, new_dim_name)
+    da_sum = sc.sum(filtered_da.bins.sum())
+
+    assert da_sum.value == 3317887.25
+
+
+def test_result_of_filter_by_time_incorrect_event_time_coord():
+    da = scn.data.tutorial_event_data()
+
+    # Run scipp filter by time
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+    half_duration = sc.to_unit(
+        sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+        unit="ns")
+    run_end = run_start + half_duration
+    with pytest.raises(sc.core.NotFoundError):
+        filter_by_time(da.copy(), run_start, run_end, "this_is_not_a_valid_time_coord_for_the_event_data")
+
+
+def test_result_of_filter_by_time_different_unit_variable_times():
+    da = scn.data.tutorial_event_data()
+
+    # Change unit to seconds in the event data
+    da.events.coords["pulse_time"] = sc.to_unit(da.events.coords["pulse_time"], unit="s")
+
+    # Run scipp filter by time
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[s]"))
+    half_duration = sc.to_unit(
+        sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+        unit="s")
+    run_end = run_start + half_duration
+    filtered_da = filter_by_time(da.copy(), run_start, run_end, datetime_unit="s")
+    da_sum = sc.sum(filtered_da.bins.sum())
+
+    # Expect a different result as we are filtering out more due to the reduced pulse_time resolution
+    assert da_sum.value == 3317864.75
+
+
+def test_result_of_filter_by_time_string_times():
+    da = scn.data.tutorial_event_data()
+
+    # Run scipp filter by time
+    filtered_da = filter_by_time(da.copy(), "2011-08-12T15:50:17", "2011-08-12T17:22:06")
+    da_sum = sc.sum(filtered_da.bins.sum())
+
+    # Should just be everything given it is passed the start - 1 second and end + 1 second
+    assert da_sum.value == 6334321.5
+    assert da_sum.value == sc.sum(da.bins.sum()).value
+
+
+def test_result_of_filter_by_time_incorrect_unit_string_times():
+    da = scn.data.tutorial_event_data()
+
+    # Run scipp filter by time
+    with pytest.raises(sc.core.UnitError):
+        filter_by_time(da.copy(), "2011-08-12T15:50:17", "2011-08-12T17:22:05", datetime_unit="s")
+
+
+def test_result_of_filter_attribute_by_time_variable():
+    da = scn.data.tutorial_event_data()
+
+    run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+    half_duration = sc.to_unit(
+        sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+        unit="ns")
+    run_end = run_start + half_duration
+
+    filter_result_first_half = filter_attribute_by_time(da, "SampleTemp", run_start, run_end)
+
+    assert len(filter_result_first_half) == 257
+    np.testing.assert_almost_equal(sc.sum(filter_result_first_half).value, 77123.02212524414)
+
+
+def test_result_of_filter_attribute_by_time_string():
+    da = scn.data.tutorial_event_data()
+
+    filter_result_full = filter_attribute_by_time(da, "SampleTemp", da.attrs["run_start"].value,
+                                                  da.attrs["end_time"].value)
+
+    assert len(filter_result_full) == 465
+    np.testing.assert_almost_equal(sc.sum(filter_result_full).value, 139523.16213989258)
+
+
+def test_result_of_filter_attribute_by_time_none_default_time():
+    da = scn.data.tutorial_event_data()
+
+    # Change SampleTemp time dimension name
+    new_dim_time = "new_time"
+    da.attrs["SampleTemp"].value.coords[new_dim_time] = da.attrs["SampleTemp"].value.coords.pop("time")
+
+    filter_result_full = filter_attribute_by_time(da, "SampleTemp", da.attrs["run_start"].value,
+                                                  da.attrs["end_time"].value, time_name=new_dim_time)
+
+    assert len(filter_result_full) == 465
+    np.testing.assert_almost_equal(sc.sum(filter_result_full).value, 139523.16213989258)
+
+
+
+
+# # Equivalent to ensuring that FilterLogByTime is possible
+# @with_mantid_only
+# def test_result_of_filter_attribute_by_time():
+#     from mantid.simpleapi import FilterLogByTime, Load
+#
+#     filename = "PG3_4866_event.nxs"
+#     ws = Load(filename)
+#     da = scn.from_mantid(ws)
+#
+#     scipp_run_start = sc.scalar(value=np.datetime64(da.attrs["run_start"].value).astype("datetime64[ns]"))
+#     scipp_run_end = scipp_run_start + sc.to_unit(
+#         sc.scalar(value=da.attrs["duration"].value * 0.5, dtype=sc.dtype.int64, unit=sc.units.s),
+#         unit="ns")
+#     da_sum = _filter_attribute_by_time(da, "SampleTemp", scipp_run_start, scipp_run_end)
+#     scipp_mean = sc.mean(da_sum.data)
+#
+#     mantid_end = da.attrs["duration"].value * 0.5
+#     mantid_results = FilterLogByTime(InputWorkspace="ws", LogName="SampleTemp", StartTime=0, EndTime=mantid_end)
+#     mantid_mean = np.mean(np.mean(mantid_results))
+#
+#     np.testing.assert_almost_equal(scipp_mean.value, mantid_mean)
+#
+#
+# # Equivalent to ensuring that it's possible to filter a log by value
+# @with_mantid_only
+# def test_result_of_filter_attribute_by_value():
+#     from mantid.simpleapi import Load
+#
+#     filename = "PG3_4866_event.nxs"
+#     ws = Load(filename)
+#     da = scn.from_mantid(ws)
+#
+#     scipp_result = _filter_attribute_by_value(da, "SampleTemp", sc.scalar(299.), sc.scalar(300.))
+#
+#     assert len(scipp_result.data.values) == 467
+#     np.testing.assert_almost_equal(sc.mean(scipp_result.data).value, 299.9960808)
+
+
+# Equivalent to ensuring that
+
+# @with_mantid_only
+# def test_result_of_bad_pulse_performance_mantid_comparison():
+#     from mantid.simpleapi import FilterBadPulses
+#
+#     scipp_centre = bench_scipp_filter(filter_bad_pulses, filename="PG3_4866_event.nxs", repeat_times=5, args=True)
+#     scipp_left = bench_scipp_filter(filter_bad_pulses, filename="PG3_4866_event.nxs", repeat_times=5)
+#     mantid_centre = bench_mantid_filter(FilterBadPulses, filename="PG3_4866_event.nxs", repeat_times=5)
+#
+#     average_scipp = (float(scipp_left) + float(scipp_centre)) / 2.
+#
+#     # Assert that Mantid is no more than 3 times faster than the average of a Scipp equivalent operation for filtering
+#     # bad pulses, this is for stability as we expect no more than 2 times as more Scipp operations need to occur.
+#     assert mantid_centre / average_scipp <= 3
+
+
+# def _filter_by_time(da, start, end):
+#     edges = sc.concatenate(start, end, 'pulse_time')
+#     return sc.bin(da, edges=[edges])
+
 
 
